6b31d868452b50a941ac8046578dfcb7c5add35f
root <kaumba@kalichipictures.com>
2024-05-03 09:32:28 -0700

    task 0- puppet is not your friend

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# this script demostrates creating a file in puppet
+
+file { '/tmp/school':
+  ensure  => 'present',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,2 @@
+This folder contains my comleted tasks in the 0x0A. Configuration management
+project of the alx programme
