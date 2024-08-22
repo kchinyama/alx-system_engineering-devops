992a7b071c751843333d20c897544f2a4f6b19cc
Kaumba Chinyama <kaumba@kalichipictures.com>
2024-08-22 11:00:00 -0700

    task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# demo of changing limit in order to allow more traffic
+
+exec { 'nginx fix-limit':
+  onlyif   => 'test -e /etc/default/nginx',
+  command  => "sed -i s/'-n 15'/'-n 4096'/g /etc/default/nginx;  service nginx restart",
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,1 @@
+This directory contains my completed tasks inside the 0x1B-web_stack_debugging_4 of the alx program
