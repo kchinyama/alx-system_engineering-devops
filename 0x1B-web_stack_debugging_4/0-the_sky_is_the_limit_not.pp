89772e980f0bef51ce5c6488831c2cbc920cd198
Kaumba Chinyama <kaumba@kalichipictures.com>
2024-08-22 11:04:26 -0700

    task 0 edits

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,5 +2,9 @@
 
 exec { 'nginx fix-limit':
   onlyif   => 'test -e /etc/default/nginx',
-  command  => "sed -i s/'-n 15'/'-n 4096'/g /etc/default/nginx;  service nginx restart",
+  command  => "sed -i s/'-n 15'/'-n 4096'/g /etc/default/nginx",
+}
+
+exec { 'restart nginx':
+  command  => "service nginx restart",
 }
